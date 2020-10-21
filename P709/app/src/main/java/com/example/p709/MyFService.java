25c7c800085aef015d69880ab399495b4cd163c8
daddimi <daddimi@gmail.com>
2020-10-21 16:26:17 +0900

    1021class

---
+++ P709/app/src/main/java/com/example/p709/MyFService.java
@@ -0,0 +1,22 @@
+package com.example.p709;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFService extends FirebaseMessagingService {
+    public MyFService() {
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        String title = remoteMessage.getNotification().getTitle();
+        String control = remoteMessage.getData().get("control");
+        String data = remoteMessage.getData().get("data");
+        Log.d("[TAG]:",title+" "+control+" "+data);
+    }
+
+}
